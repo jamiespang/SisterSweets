c8e40b0481ccc54641729968658e67b84f4b092d
Jamie Spang <jamie_spang@yahoo.com>
2022-03-12 14:13:53 -0500

    added JS file and updated CSS

---
+++ app.js
@@ -0,0 +1,23 @@
+// hamburger menu
+const toggleButton = document.getElementById('hamburger');
+const navbarLinks = document.getElementById('navbar');
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+});
+
+function closeMenu() {
+    navbarLinks.classList.toggle('active');
+}
+
+
+
+
+
+
+// submit button
+const submitBtn = document.getElementById('btn');
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    submitBtn.innerText = 'Submitted!';
+});
