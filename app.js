7186e40990052dffea76aa95fb2c759a72569a47
Jamie Spang <jamie_spang@yahoo.com>
2022-03-29 18:58:39 -0400

    styled the submit button in the form container and tested

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 window.addEventListener('load', (Event) => {
     console.log('page is fully loaded');
 });
-}
 
 function myFunction(x) {
     x.classList.toggle("change");
+    document.getElementById('Jamie').classList.toggle("show-menu");
+};
 
-}
 //verify email//
 
-function isEmail(email) {
-    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    return regex.test(String(email).toLowerCase());
-}
-// submit button//
-const submit = document.getElementById('submit');
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    submit.innerText = 'Submitted!';
-});
 
-fname
+// submit button//
+
+function submitEmail(element) {
+    element.innerHTML = "Submitted!";
+}
