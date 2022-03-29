90ecb3f9ef094f3123a330a6d63e8816a2499cb6
Jamie Spang <jamie_spang@yahoo.com>
2022-03-28 21:18:58 -0400

    added CSS and JS and got media queries to work

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-<button id = "hamburgerBtn">& #9776;</button>
-<ul id="primaryNav">
-
-function toggleMenu(){
-document.getElementById("primaryNav").classList.toggle("open"); 
+window.addEventListener('load', (Event) => {
+    console.log('page is fully loaded');
+});
 }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+// submit button//
+const submit = document.getElementById('submit');
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    submit.innerText = 'Submitted!';
+});
